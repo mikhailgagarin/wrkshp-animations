65c507243bb398653ddd8c3f99329299e8414705
Mike Gagarin <m.gagarin@paralect.com>
2020-02-21 19:36:42 +0300

    After Workshop

--- src/components/Fade/index.jsx
+++ src/components/Fade/index.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 
 import * as S from './styles';
 
 function Fade({isShown, children}) {
-    return (
-        <S.Fade isShown={isShown}>
-            {children}
-        </S.Fade>
-    );
+    const [on, toggle] = useState(false);
+
+    const onAnimationEnd = useCallback(() => {
+        if (!isShown) {
+            toggle(false);
+        }
+    }, [isShown]);
+
+    useEffect(() => {
+        if (isShown) {
+            toggle(true);
+        }
+    }, [isShown]);
+
+    if (on) {
+        return (
+            <S.Fade isShown={isShown} onAnimationEnd={onAnimationEnd}>
+                {children}
+            </S.Fade>
+        );
+    }
+
+    return null;
 }
 
 export default Fade;
--- src/pages/Basic/index.js
+++ src/pages/Basic/index.js
@@ -1,22 +1,53 @@
 import React, {useState} from 'react';
+import {animated, useSpring, config} from 'react-spring';
 
 import {ButtonPrimary} from 'components/Button';
 import Fade from 'components/Fade';
 
-export default function Basic() {
-    const [on, toggle] = useState(false);
+const AnimatedButton = animated(ButtonPrimary);
 
+export default function Basic() {
+    const [on, toggle] = useState(true);
+    // const {number} = useSpring({
+    //     number: 100, from: {number: 0},
+    //     config: config.slow,
+    // });
+
+    // return (
+    //     <>
+    //         <h1>Basic usage showcase</h1>
+
+    //         {/* <ButtonPrimary
+    //             onClick={() => toggle(!on)}
+    //         >
+    //             {on ? 'Hide' : 'Show'}
+    //         </ButtonPrimary> */}
+
+    //         <animated.div>{number}</animated.div>
+
+    //         <div>Foo Bar</div>
+    //     </>
+    // );
+
+    const props = useSpring({
+        x: on ? 1 : 0,
+        config: config.slow,
+    })
     return (
         <>
-            <h1>Basic usage showcase</h1>
-
-            <ButtonPrimary onClick={() => toggle(!on)}>
+            <AnimatedButton
+                onClick={() => toggle(!on)}
+                style={{
+                    transform: props.x
+                        .interpolate({
+                            range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+                            output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1]
+                        })
+                        .interpolate(x => `scale(${x})`)
+                    }}
+            >
                 {on ? 'Hide' : 'Show'}
-            </ButtonPrimary>
-
-            <Fade isShown={on}>
-                <div>HELLO WORLD!</div>
-            </Fade>
+            </AnimatedButton>
         </>
-    );
+    )
 }
